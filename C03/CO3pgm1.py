4c4d5e587209d1e6a28cdc5630afc98d62c18303
amaltommits <148201972+amaltommits@users.noreply.github.com>
2024-01-02 17:05:05 +0530

    Create CO3pgm1.py

---
+++ C03/CO3pgm1.py
@@ -0,0 +1,35 @@
+# Python comes with a comprehensive standard library that includes a 
+# wide range of built-in packages and modules. These modules provide 
+# functionality for tasks ranging from file I/O to web development. Here 
+# are some commonly used built-in packages in Python:
+# 1. os : Operating system interface, provides a way of using operating 
+# system-dependent functionality like reading or writing to the file system.
+# import os
+# 2. sys : Provides access to some variables used or maintained by the 
+# interpreter and to functions that interact strongly with the interpreter.
+# import sys
+# 3. math : Mathematical functions such as basic arithmetic operations, 
+# logarithms, trigonometric functions, etc.
+#  import math
+# 4. datetime : Date and time handling.
+#  import datetime
+# 5. json : JSON encoder and decoder.
+#  import json
+# 6. urllib : URL handling modules, including parsing, quoting, and fetching.
+#  from urllib import request, parse
+# 7. random : Generate pseudo-random numbers.
+#  import random
+# 8. re : Regular expression operations.
+#  import re
+# 9. collections : Implements specialized container data types.
+#  from collections import Counter, defaultdict
+# 10. sqlite3 : SQLite database interface.
+#  import sqlite3
+# 11. csv : CSV file reading and writing.
+#  import csv
+# 12. gzip : Support for gzip files.
+#  import gzip
+# 13. socket : Low-level networking interface.
+#  import socket
+# 14. argparse : Command-line argument parsing.
+# import argparse
