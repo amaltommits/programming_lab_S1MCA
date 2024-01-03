47999d83b398b0c0ef0858ad59e502aa65dfaaac
amaltommits <148201972+amaltommits@users.noreply.github.com>
2024-01-03 15:19:19 +0530

    Update pgm14.py

--- CO1/pgm14.py
+++ CO1/pgm14.py
@@ -1,10 +1,8 @@
 # Print out all colors from color-list1 not contained in color-list2. 
-n=int(input("Enter the no of colors you want to insert:"))
-clist=[]
-print("Enter the color:")
-for i in range(n):
- color=input()
-clist.append(color)
-print(clist)
-print("The first and last colors are:")
-print(clist[0],clist[-1])
+
+color_list1 = input("Enter colors for color-list1, separated by commas: ").split(',')
+color_list2 = input("Enter colors for color-list2, separated by commas: ").split(',')
+differing_colors = [color.strip() for color in color_list1 if color.strip() not in color_list2]
+print("Colors in color-list1 not contained in color-list2:")
+for color in differing_colors:
+    print(color)
